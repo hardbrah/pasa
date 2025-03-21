dec2fd649ec57ca8677261fc931fa3aaa60afa1c
hardbrah <1713476245@qq.com>
2025-03-21 14:56:40 +0800

    实现positive样例的测试

--- test_selector/test_by_gpt4o.py
+++ test_selector/test_by_gpt4o.py
@@ -3,20 +3,24 @@ from openai import OpenAI
 import sys
 import os
 import json 
+import logging
+client = OpenAI(
+    api_key='EMPTY',
+    base_url='http://localhost:8000/v1',
+)
 
-# # 获取目标模块所在的绝对路径
-# module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "my_module_directory"))
-# sys.path.append(module_path)
+# Set up logging
+from datetime import datetime
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+logging.basicConfig(
+    filename=f"./log/process_log_{timestamp}.txt", 
+    level=logging.INFO, 
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    encoding='utf-8' 
+)
 
-# 现在可以导入该目录中的模块
-# import my_module
-
-
-# client = OpenAI(
-#     api_key='EMPTY',
-#     base_url='http://localhost:8000/v1',
-# )
-
+right_predict = 0
+all_predict = 0
 def process_queries():
     try:
         with open('./prompt/selector_p2.txt', 'r', encoding='utf-8') as f:
@@ -24,8 +28,6 @@ def process_queries():
     except FileNotFoundError:
         logging.error("Unable to read the prompt file selector_p2.txt!")
         return None
-
-    
 
     with open("../decision/q_true.txt", "r") as f:
         true_file = f.read().splitlines()
@@ -50,7 +52,7 @@ def process_queries():
         )
         messages = [{
             'role': 'user',
-            'content': query
+            'content': prompt
         }]
 
         resp = client.chat.completions.create(
@@ -59,25 +61,17 @@ def process_queries():
             seed=42)
         response = resp.choices[0].message.content
 
+        with open(f"./response/qwen_response_{timestamp}.txt", 'w') as f:
+            f.write(prompt + '\n' + response + '\n' + true_file[i])
+        
+
         # 找到 "**Decision:**" 后面的值
         first_line = response.split("\n")[0]  # 获取第一行
         decision_value = first_line.split("**Decision:**")[1].strip()  # 提取 "True"
-        if(decision_value == decision.strip()) print("True")
+        if(decision_value == decision.strip()):
+            right_predict += 1
+            print("True")
+        all_predict += 1
 
 if __name__ == "__main__":
     process_queries()    
-
-# query = 'You are an elite researcher in the field of AI, conducting research on Can you provide works that used Graph Neural Networks (GNNs) to integrate node representations between graphs or construct an instance graph for multi-view data?. Evaluate whether the following paper fully satisfies the detailed requirements of the user query and provide your reasoning. Ensure that your decision and reasoning are consistent. Searched Paper: Title: Low Data Drug Discovery with One-shot Learning Abstract: Recent advances in machine learning have made significant contributions to drug discovery. Deep neural networks in particular have been demonstrated to provide significant boosts in predictive power when inferring the properties and activities of small-molecule compounds. However, the applicability of these techniques has been limited by the requirement for large amounts of training data. In this work, we demonstrate how one-shot learning can be used to significantly lower the amounts of data required to make meaningful predictions in drug discovery applications. We introduce a new architecture, the residual LSTM embedding, that, when combined with graph convolutional neural networks, significantly improves the ability to learn meaningful distance metrics over small-molecules. We open source all models introduced in this work as part of DeepChem, an open-source framework for deep-learning in drug discovery. User Query: Can you provide works that used Graph Neural Networks (GNNs) to integrate node representations between graphs or construct an instance graph for multi-view data? Output format: Decision: True/False Reason:... Decision:'
-# messages = [{
-#     'role': 'user',
-#     'content': query
-# }]
-# resp = client.chat.completions.create(
-#     model='Qwen2.5-7B-Instruct', # 注意这里的default-lora，代表使用lora进行推理，也可以使用qwen2-7b-instruct，即使用原模型了，下同
-#     messages=messages,
-#     seed=42)
-# response = resp.choices[0].message.content
-# print(f'query: {query}')
-# print(f'response: {response}')
-# # available_models = client.models.list()
-# # print([model.id for model in available_models])
